62682724c92c62063c570602c6468d88daef7344
aivann <apobutial@gmail.com>
2025-02-15 20:59:13 +0800

    Modified LoadUserEvents

--- Eventful/event_manager/src/main/java/com/btp/event_manager/service/LoadUserEvents.java
+++ Eventful/event_manager/src/main/java/com/btp/event_manager/service/LoadUserEvents.java
@@ -12,19 +12,23 @@ import java.time.LocalDateTime;
 // Eventful/dat/[username].xml
 public class LoadUserEvents {
     public static void load(AppService appService) {
-        NodeList events = ReadEventsService.read(appService);
-        for (int i = 0; i < events.getLength(); i++) {
-            Element event = (Element) events.item(i);
+        try {
+            NodeList events = ReadEventsService.read(appService);
+            for (int i = 0; i < events.getLength(); i++) {
+                Element event = (Element) events.item(i);
 
-            String eventName = event.getElementsByTagName("title").item(0).getTextContent();
-            LocalDate startDate = LocalDate.parse(event.getElementsByTagName("startDate").item(0).getTextContent());
-            LocalDate endDate = LocalDate.parse(event.getElementsByTagName("endDate").item(0).getTextContent());
-            LocalDateTime lastAccessed = LocalDateTime.parse(event.getElementsByTagName("lastAccessed").item(0).getTextContent());
+                String eventName = event.getElementsByTagName("title").item(0).getTextContent();
+                LocalDate startDate = LocalDate.parse(event.getElementsByTagName("startDate").item(0).getTextContent());
+                LocalDate endDate = LocalDate.parse(event.getElementsByTagName("endDate").item(0).getTextContent());
+                LocalDateTime lastAccessed = LocalDateTime.parse(event.getElementsByTagName("lastAccessed").item(0).getTextContent());
 
-            Event savedEvent = new Event(eventName, startDate, endDate);
-            savedEvent.setLastAccessed(lastAccessed);
+                Event savedEvent = new Event(eventName, startDate, endDate);
+                savedEvent.setLastAccessed(lastAccessed);
 
-            appService.getCurrUser().getEvents().add(savedEvent);
+                appService.getCurrUser().getEvents().add(savedEvent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
