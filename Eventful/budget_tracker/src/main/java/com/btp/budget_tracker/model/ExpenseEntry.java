cafccb024798b4a78eb904b18a656c3adb80f471
aivann <apobutial@gmail.com>
2025-03-01 15:37:19 +0800

    Updated files

--- Eventful/budget_tracker/src/main/java/com/btp/budget_tracker/model/ExpenseEntry.java
+++ Eventful/budget_tracker/src/main/java/com/btp/budget_tracker/model/ExpenseEntry.java
@@ -4,7 +4,47 @@ import lombok.Data;
 
 @Data
 public class ExpenseEntry {
-    private int quantity; // must be greater than or equal to 1
+    private int quantity;
     private String itemName;
-    private Double cost; // should not be negative
+    private double costPerItem;
+
+    public ExpenseEntry() {
+        this.quantity = 1;
+        this.itemName = "";
+        this.costPerItem = 0.0;
+    }
+
+    public ExpenseEntry(int quantity, String itemName, double costPerItem) {
+        this.quantity = Math.max(quantity, 1);
+        this.itemName = itemName;
+        this.costPerItem = Math.max(costPerItem, 0.0);
+    }
+
+    public double getTotalCost() {
+        return quantity * costPerItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getCostPerItem() {
+        return costPerItem;
+    }
+
+    public void setCostPerItem(double costPerItem) {
+        this.costPerItem = costPerItem;
+    }
 }
